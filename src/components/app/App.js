e60a6685fa029823ef0f5fb74306f49629963670
Michal <ndwz123@gmail.com>
2019-08-02 15:32:58 +0200

    Move to class component and add image prerender of upload

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,40 +1,58 @@
-import React from "react";
+import React, {Component} from "react";
 import Header from "../header/Header";
 import * as Tesseract from "tesseract.js";
 import "./App.css";
 
 const title = "Quote Keeper";
 
-const handleChange = e => {
-  const files = e.target.files,
-    uploads = [];
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-  if (files && files.length) {
-    for (let key in files) {
-      if (!files.hasOwnProperty(key)) continue;
-
-      uploads.push(URL.createObjectURL(files[key]));
-    }
+    this.state = {
+      uploads: []
+    };
   }
-};
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header title={title} />
-      <section>
-        <label className="fileUploaderContainer">
-          <input
-            id="fileUploader"
-            type="file"
-            onChange={handleChange}
-            multiple
-          />
-        </label>
-        <div />
-      </section>
-    </div>
-  );
-};
+  handleChange = e => {
+    const files = e.target.files,
+      uploads = [];
+
+    if (files && files.length) {
+      for (let key in files) {
+        if (!files.hasOwnProperty(key)) continue;
+
+        uploads.push(URL.createObjectURL(files[key]));
+
+        this.setState({uploads});
+      }
+    } else {
+      this.setState({uloads: []});
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header title={title} />
+        <section>
+          <label className="fileUploaderContainer">
+            <input
+              id="fileUploader"
+              type="file"
+              onChange={this.handleChange}
+              multiple
+            />
+          </label>
+          <div>
+            {this.state.uploads.map((src, index) => {
+              return <img src={src} key={index} width="400px" />;
+            })}
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
 
 export default App;
