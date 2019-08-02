51bf7a7990950b3ad9b17f558b008fa3ad10064e
Michal <ndwz123@gmail.com>
2019-08-02 15:09:52 +0200

    Create handleChange method for uploaded input

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,15 +3,33 @@ import Header from "../header/Header";
 import * as Tesseract from "tesseract.js";
 import "./App.css";
 
-const App = () => {
-  const title = "Quote Keeper";
+const title = "Quote Keeper";
 
+const handleChange = e => {
+  const files = e.target.files,
+    uploads = [];
+
+  if (files && files.length) {
+    for (let key in files) {
+      if (!files.hasOwnProperty(key)) continue;
+
+      uploads.push(URL.createObjectURL(files[key]));
+    }
+  }
+};
+
+const App = () => {
   return (
     <div className="App">
       <Header title={title} />
       <section>
         <label className="fileUploaderContainer">
-          <input id="fileUploader" type="file" multiple />
+          <input
+            id="fileUploader"
+            type="file"
+            onChange={handleChange}
+            multiple
+          />
         </label>
         <div />
       </section>
